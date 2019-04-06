1a1bd5ab2b33b91eff4e1a1c8fc663404dfccfe8
Alexander Sharov <kvendingoldo@yandex.ru>
2019-04-07 00:05:58 +0400

    Add detailed view

--- src/main/python/phi/web/urls.py
+++ src/main/python/phi/web/urls.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import include, url
 
 from . import views
 
 urlpatterns = [
-    url('index_files', views.index_files, name='index_files'),
+    re_path(r'documents', views.DocumentsView.as_view(), name='list'),
+    re_path(r'^document/(?P<pk>.+)$', views.DocumentView.as_view(), name='document-detail'),
+
+
+
     url('index', views.index, name='index'),
     url('upload', views.upload, name='upload'),
     url('preview', views.preview, name='preview'),
     path('admin/', admin.site.urls),
     url(r'^register/$', views.register, name='register'),
     url(r'^user_login/$',views.user_login,name='user_login'),
-    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^logout/$', views.user_logout, name='logout')
 ]
